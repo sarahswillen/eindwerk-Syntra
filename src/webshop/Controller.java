eeb8fa91b711c74cdc4e450795d5e4f01414debc
Rinaldo <menichettir@hotmail.com>
2021-05-11 21:45:00 +0200

    toevoegen categorie layouot

--- src/webshop/Controller.java
+++ src/webshop/Controller.java
@@ -11,49 +11,43 @@ public class Controller {
     private Label label;
 
     @FXML
-    private BorderPane mainPaine;
+    private BorderPane mainPane;
 
     @FXML
     private void handleButtonCategorie(ActionEvent event){
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("Categorie");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
     }
 
     public void handleButtonArtikels(ActionEvent actionEvent) {
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("Artikels");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
     }
 
     public void handleButtonKlanten(ActionEvent actionEvent) {
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("Klanten");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
     }
 
     public void handleButtonReviews(ActionEvent actionEvent) {
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("Reviews");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
     }
 
     public void handleButtonActieCodes(ActionEvent actionEvent) {
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("ActieCode");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
     }
 
     public void handleButtonDashboard(ActionEvent actionEvent) {
-        System.out.println("you clicked me");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPage("Dashboard");
-        mainPaine.setCenter(view);
+        mainPane.setCenter(view);
 
     }
 }
